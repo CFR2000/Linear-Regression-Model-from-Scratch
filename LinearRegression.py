b5aede18d682ff68f908851b4a9987855db671c2
Colm_Rooney <77832627+CFR2000@users.noreply.github.com>
2022-12-26 18:32:38 +0000

    defining sketal framework for model

---
+++ LinearRegression.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+
+
+# *** An optimization algorithm is used to update the model's parameters        *** #
+# *** in order to minimize the loss.  Common optimization algorithms for linear *** #
+# *** regression include gradient descent and stochastic gradient descent.      *** #
+
+def sigmoid():
+    pass
+
+def tanh():
+    pass
+
+def relu():
+    pass
+
+class LR:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+    def forward(self):
+        pass
+
+    def backward(self):
+        pass
+
+    def loss():
+        pass
+
+    
